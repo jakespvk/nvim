30a190bf0fa13c5de73f2a20ea417a7b1bf18105
Jake Spievak <jakespvk@gmail.com>
2023-04-22 16:12:42 -0700

    theme updates

--- init.lua
+++ init.lua
@@ -15,8 +15,15 @@ return require('packer').startup(function(use)
     }
 
     use ('tjdevries/colorbuddy.nvim')
+    use ('folke/lsp-colors.nvim')
     -- theme
-    use ('ellisonleao/gruvbox.nvim') 
+    use ('folke/tokyonight.nvim')
+    --use({ 'rose-pine/neovim', as = 'rose-pine' })
+    --use 'Mofiqul/dracula.nvim'
+    --use { 'uloco/bluloco.nvim', requires = { 'rktjmp/lush.nvim' } }
+    --use {'nyoom-engineering/oxocarbon.nvim'}
+    -- use ('Shatur/neovim-ayu')
+    --use ('ellisonleao/gruvbox.nvim')
 
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use ('nvim-treesitter/playground')
@@ -28,6 +35,83 @@ return require('packer').startup(function(use)
     use ('lewis6991/gitsigns.nvim')
     use ('folke/neodev.nvim')
     use ('theprimeagen/vim-be-good')
+
+    -- todo comments
+    -- Lua
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+            require("todo-comments").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+                -- signs = true, -- show icons in the signs column
+                sign_priority = 8, -- sign priority
+                -- keywords recognized as todo comments
+                keywords = {
+                    FIX = {
+                        icon = " ", -- icon used for the sign, and in search results
+                        color = "error", -- can be a hex color, or a named color (see below)
+                        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                        -- signs = false, -- configure signs for some keywords individually
+                    },
+                    TODO = { icon = " ", color = "info" },
+                    HACK = { icon = " ", color = "warning" },
+                    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+                },
+                gui_style = {
+                    fg = "NONE", -- The gui style to use for the fg highlight group.
+                    bg = "BOLD", -- The gui style to use for the bg highlight group.
+                },
+                merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+                -- highlighting of the line containing the todo comment
+                -- * before: highlights before the keyword (typically comment characters)
+                -- * keyword: highlights of the keyword
+                -- * after: highlights after the keyword (todo text)
+                highlight = {
+                    multiline = true, -- enable multine todo comments
+                    multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+                    multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+                    before = "", -- "fg" or "bg" or empty
+                    keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                    after = "fg", -- "fg" or "bg" or empty
+                    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+                    comments_only = true, -- uses treesitter to match keywords in comments only
+                    max_line_len = 400, -- ignore lines longer than this
+                    exclude = {}, -- list of file types to exclude highlighting
+                },
+                -- list of named colors where we try to extract the guifg from the
+                -- list of highlight groups or use the hex color if hl not found as a fallback
+                colors = {
+                    error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+                    warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+                    info = { "DiagnosticInfo", "#2563EB" },
+                    hint = { "DiagnosticHint", "#10B981" },
+                    default = { "Identifier", "#7C3AED" },
+                    test = { "Identifier", "#FF00FF" }
+                },
+                search = {
+                    command = "rg",
+                    args = {
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                    },
+                    -- regex that will be used to match keywords.
+                    -- don't replace the (KEYWORDS) placeholder
+                    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+                    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+                },
+
+            }
+        end
+    }
 
     -- theming below
     use {
@@ -65,8 +149,8 @@ return require('packer').startup(function(use)
         options = {
             icons_enabled = true,
             theme = 'auto',
-            component_separators = { none },
-            section_separators = { none },
+            component_separators = { },
+            section_separators = { },
             disabled_filetypes = {
                 statusline = {},
                 winbar = {},
@@ -154,6 +238,10 @@ vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- set.lua
+
+--fat cursor !!!
+--vim.opt.guicursor = ''
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -164,11 +252,12 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+-- word wrap
+-- --vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("AppData/Local/nvim") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -188,7 +277,12 @@ vim.g.mapleader = " "
 
 -- colors.lua
 -- vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+--vim.cmd([[colorscheme gruvbox]])
+--vim.cmd([[colorscheme ayu]])
+--vim.cmd([[colorscheme oxocarbon]])
+--vim.cmd([[colorscheme dracula]])
+--vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme tokyonight')
 --  * highlight SignColumn guibg=NONE
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
