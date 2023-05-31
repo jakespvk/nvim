e1c3516836b1ad50cb22e258208bec7d31fe570c
Jake Spievak <jakespvk@gmail.com>
2023-05-30 19:21:25 -0700

    fix: theme hopping

--- init.lua
+++ init.lua
@@ -17,15 +17,9 @@ return require('packer').startup(function(use)
     use ('tjdevries/colorbuddy.nvim')
     use ('folke/lsp-colors.nvim')
     -- theme
-    --use { 'kartikp10/noctis.nvim', requires = { 'rktjmp/lush.nvim' } }
-    --use ('CrispyBaccoon/gruvboxed')
-    use "EdenEast/nightfox.nvim" -- Packer
-    --use { "bluz71/vim-moonfly-colors", as = "moonfly" }
-    --use({ 'rose-pine/neovim', as = 'rose-pine' })
-    --use { 'uloco/bluloco.nvim', requires = { 'rktjmp/lush.nvim' } }
-    --use {'nyoom-engineering/oxocarbon.nvim'}
-    --use ('ellisonleao/gruvbox.nvim')
-    --use ('sainnhe/gruvbox-material')
+    --use 'tomasiser/vim-code-dark'
+    --use "EdenEast/nightfox.nvim" -- Packer
+    use ('ellisonleao/gruvbox.nvim')
 
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use ('nvim-treesitter/nvim-treesitter-context')
@@ -937,68 +931,78 @@ vim.diagnostic.config({
 
 -- colors.lua
 
-require('nightfox').setup({
-  options = {
-    -- Compiled file's destination location
-    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-    compile_file_suffix = "_compiled", -- Compiled file suffix
-    transparent = true,     -- Disable setting background
-    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,    -- Non focused panes set to alternative background
-    module_default = true,   -- Default enable value for modules
-    colorblind = {
-      enable = false,        -- Enable colorblind support
-      simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-      severity = {
-        protan = 0,          -- Severity [0,1] for protan (red)
-        deutan = 0,          -- Severity [0,1] for deutan (green)
-        tritan = 0,          -- Severity [0,1] for tritan (blue)
-      },
-    },
-    styles = {               -- Style to be applied to different syntax groups
-      comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "NONE",
-      constants = "NONE",
-      functions = "NONE",
-      keywords = "NONE",
-      numbers = "NONE",
-      operators = "NONE",
-      strings = "NONE",
-      types = "NONE",
-      variables = "NONE",
-    },
-    inverse = {             -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    modules = {             -- List of various plugins and additional options
-      -- ...
-    },
-  },
-  palettes = {},
-  specs = {},
-  groups = {},
-})
+--require('nightfox').setup({
+--  options = {
+--    -- Compiled file's destination location
+--    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+--    compile_file_suffix = "_compiled", -- Compiled file suffix
+--    transparent = true,     -- Disable setting background
+--    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+--    dim_inactive = false,    -- Non focused panes set to alternative background
+--    module_default = true,   -- Default enable value for modules
+--    colorblind = {
+--      enable = false,        -- Enable colorblind support
+--      simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
+--      severity = {
+--        protan = 0,          -- Severity [0,1] for protan (red)
+--        deutan = 0,          -- Severity [0,1] for deutan (green)
+--        tritan = 0,          -- Severity [0,1] for tritan (blue)
+--      },
+--    },
+--    styles = {               -- Style to be applied to different syntax groups
+--      comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
+--      conditionals = "NONE",
+--      constants = "NONE",
+--      functions = "NONE",
+--      keywords = "NONE",
+--      numbers = "NONE",
+--      operators = "NONE",
+--      strings = "NONE",
+--      types = "NONE",
+--      variables = "NONE",
+--    },
+--    inverse = {             -- Inverse highlight for different types
+--      match_paren = false,
+--      visual = false,
+--      search = false,
+--    },
+--    modules = {             -- List of various plugins and additional options
+--      -- ...
+--    },
+--  },
+--  palettes = {},
+--  specs = {},
+--  groups = {},
+--})
 
 -- setup must be called before loading
-vim.cmd("colorscheme terafox")
+--vim.cmd("colorscheme codedark")
 
---require("bluloco").setup({
---  style = "dark",               -- "auto" | "dark" | "light"
---  transparent = true,
---  italics = false,
---  terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
---  guicursor = false,
---})
---
---vim.cmd('colorscheme bluloco')
-
---vim.cmd([[colorscheme gruvboxed]])
---vim.cmd([[colorscheme noctis]])
---vim.cmd([[colorscheme oxocarbon]])
---vim.cmd('colorscheme rose-pine')
---vim.cmd('colorscheme moonfly')
+vim.o.background = "dark"
+-- setup must be called before loading the colorscheme
+-- Default options:
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = false,
+    comments = false,
+    operators = false,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
 --  :highlight SignColumn guibg=NONE
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
