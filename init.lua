886eb27589d3c4983a81cf75f62591268b596a58
Jake Spievak <jakespvk@gmail.com>
2024-02-18 22:03:02 -0800

    need to fix: lualine setup not working

--- init.lua
+++ init.lua
@@ -148,10 +148,9 @@ require("lazy").setup({
         end
     },
 
-    'nvim-lualine/lualine.nvim',
     {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'kyazdani42/nvim-web-devicons', lazy = false }
+        dependencies = { 'kyazdani42/nvim-web-devicons' }
     },
 
     -- ^^ hopefully theming
@@ -180,46 +179,46 @@ require("lazy").setup({
         }
     },
 
-    require('lualine').setup {
-        options = {
-            icons_enabled = true,
-            theme = 'auto',
-            component_separators = { },
-            section_separators = { },
-            disabled_filetypes = {
-                statusline = {},
-                winbar = {},
-            },
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            }
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {'filename'},
-            lualine_x = {'encoding', 'fileformat', 'filetype'},
-            lualine_y = {'progress'},
-            lualine_z = {'location'}
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    },
+    --require('lualine').setup {
+    --    options = {
+    --        icons_enabled = true,
+    --        theme = 'auto',
+    --        component_separators = { },
+    --        section_separators = { },
+    --        disabled_filetypes = {
+    --            statusline = {},
+    --            winbar = {},
+    --        },
+    --        ignore_focus = {},
+    --        always_divide_middle = true,
+    --        globalstatus = false,
+    --        refresh = {
+    --            statusline = 1000,
+    --            tabline = 1000,
+    --            winbar = 1000,
+    --        }
+    --    },
+    --    sections = {
+    --        lualine_a = {'mode'},
+    --        lualine_b = {'branch', 'diff', 'diagnostics'},
+    --        lualine_c = {'filename'},
+    --        lualine_x = {'encoding', 'fileformat', 'filetype'},
+    --        lualine_y = {'progress'},
+    --        lualine_z = {'location'}
+    --    },
+    --    inactive_sections = {
+    --        lualine_a = {},
+    --        lualine_b = {},
+    --        lualine_c = {'filename'},
+    --        lualine_x = {'location'},
+    --        lualine_y = {},
+    --        lualine_z = {}
+    --    },
+    --    tabline = {},
+    --    winbar = {},
+    --    inactive_winbar = {},
+    --    extensions = {}
+    --},
 
     --require('nvim-ts-autotag').setup ()
 })
