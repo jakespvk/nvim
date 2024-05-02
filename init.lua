2df5adf70765867941b80d022959c7f8a1f819f3
Jake Spievak <jakespvk@gmail.com>
2024-05-02 10:04:08 -0700

    NO AUTOCOMMENTS AND C-c WORKS PROPERLY I WAS MISSING A BRACKET LOL

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,7 +17,8 @@ vim.g.maplocalleader = ' '
 require("lazy").setup({
 
     {
-        'nvim-telescope/telescope.nvim', version = '0.1.5',
+        'nvim-telescope/telescope.nvim',
+        version = '0.1.5',
         -- or                            , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
@@ -28,7 +29,7 @@ require("lazy").setup({
     --'tomasiser/vim-code-dark',
     --"EdenEast/nightfox.nvim" -- Packer,
     ('ellisonleao/gruvbox.nvim'),
-    { "rose-pine/neovim", name = "rose-pine" },
+    { "rose-pine/neovim",      name = "rose-pine" },
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -43,6 +44,9 @@ require("lazy").setup({
 
     -- autotag
     --('windwp/nvim-ts-autotag'),
+
+    -- centerpad
+    ('smithbm2316/centerpad.nvim'),
 
     -- git blame
     ('f-person/git-blame.nvim'),
@@ -61,9 +65,9 @@ require("lazy").setup({
     }),
 
     {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { {"nvim-lua/plenary.nvim"} }
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { { "nvim-lua/plenary.nvim" } }
     },
     ('mbbill/undotree'),
     ('tpope/vim-fugitive'),
@@ -99,8 +103,8 @@ require("lazy").setup({
                     TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
                 },
                 gui_style = {
-                    fg = "NONE", -- The gui style to use for the fg highlight group.
-                    bg = "BOLD", -- The gui style to use for the bg highlight group.
+                    fg = "NONE",       -- The gui style to use for the fg highlight group.
+                    bg = "BOLD",       -- The gui style to use for the bg highlight group.
                 },
                 merge_keywords = true, -- when true, custom keywords will be merged with the defaults
                 -- highlighting of the line containing the todo comment
@@ -108,16 +112,16 @@ require("lazy").setup({
                 -- * keyword: highlights of the keyword
                 -- * after: highlights after the keyword (todo text)
                 highlight = {
-                    multiline = true, -- enable multine todo comments
-                    multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
-                    multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-                    before = "", -- "fg" or "bg" or empty
-                    keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-                    after = "fg", -- "fg" or "bg" or empty
+                    multiline = true,                -- enable multine todo comments
+                    multiline_pattern = "^.",        -- lua pattern to match the next multiline from the start of the matched keyword
+                    multiline_context = 10,          -- extra lines that will be re-evaluated when changing a line
+                    before = "",                     -- "fg" or "bg" or empty
+                    keyword = "wide",                -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                    after = "fg",                    -- "fg" or "bg" or empty
                     pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-                    comments_only = true, -- uses treesitter to match keywords in comments only
-                    max_line_len = 400, -- ignore lines longer than this
-                    exclude = {}, -- list of file types to exclude highlighting
+                    comments_only = true,            -- uses treesitter to match keywords in comments only
+                    max_line_len = 400,              -- ignore lines longer than this
+                    exclude = {},                    -- list of file types to exclude highlighting
                 },
                 -- list of named colors where we try to extract the guifg from the
                 -- list of highlight groups or use the hex color if hl not found as a fallback
@@ -169,21 +173,21 @@ require("lazy").setup({
         branch = 'v3.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' },           -- Required
+            { 'williamboman/mason.nvim' },         -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            { 'hrsh7th/nvim-cmp' },       -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },   -- Required
+            { 'hrsh7th/cmp-buffer' },     -- Optional
+            { 'hrsh7th/cmp-path' },       -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },   -- Optional
 
             -- Snippets
-            {'L3MON4D3/LuaSnip', version = "v2.x"},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
+            { 'L3MON4D3/LuaSnip',                 version = "v2.x" }, -- Required
+            { 'rafamadriz/friendly-snippets' },     -- Optional
         }
     },
 
@@ -214,17 +218,19 @@ require('lualine').setup {
     },
     sections = {
         lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = { {'filename', path=3} },
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 3 } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        },
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {}
     },
@@ -238,6 +244,14 @@ require('lualine').setup {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- vim options (remove auto comment)
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.formatoptions:remove({ 'r', 'o' })
+    end,
+})
 
 -- remap.lua
 
@@ -254,13 +268,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 --greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
@@ -273,7 +280,7 @@ vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 -- this is going to get me cancelled
-vim.keymap.set("i", "<C-c", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- unmap arrow keys for kinesis kb
 vim.keymap.set("x", "<Up>", "<nop>")
@@ -310,7 +317,6 @@ vim.opt.smartindent = true
 
 -- word wrap
 vim.opt.wrap = false
-vim.opt.smoothscroll = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -333,18 +339,18 @@ vim.opt.isfname:append("@-@")
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', nbsp = '␣' }
+--vim.opt.list = true
+--vim.opt.listchars = { tab = '» ', nbsp = '␣' }
 
 -- Show which line your cursor is on
 --vim.opt.cursorline = true
@@ -388,32 +394,32 @@ vim.g.mapleader = " "
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'quick_lint_js', 'rust_analyzer', 'cssls', 'eslint', 'html'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+    ensure_installed = { 'quick_lint_js', 'rust_analyzer', 'cssls', 'eslint', 'html' },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    }
 })
 -- quick-lint-js
 require('lspconfig').quick_lint_js.setup {}
@@ -422,27 +428,27 @@ require('lspconfig').eslint.setup {}
 require('lspconfig').html.setup {}
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip', keyword_length = 2 },
+        { name = 'buffer',  keyword_length = 3 },
+    },
+    formatting = lsp_zero.cmp_format(),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
 })
 
 vim.diagnostic.config({
@@ -453,10 +459,10 @@ vim.diagnostic.config({
 vim.o.background = "dark"
 
 require('nightfox').setup({
-  options = {
-    transparent = true,     -- Disable setting background
-    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-  },
+    options = {
+        transparent = true, -- Disable setting background
+        terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    },
 })
 require("rose-pine").setup({
     variant = 'moon',
@@ -467,25 +473,25 @@ require("rose-pine").setup({
     },
 })
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = false,
-    comments = false,
-    operators = false,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "soft", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = true,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = false,
+        comments = false,
+        operators = false,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true,  -- invert background for search, diffs, statuslines and errors
+    contrast = "soft", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = true,
 })
 require("tokyonight").setup({
     transparent = true,
@@ -525,14 +531,14 @@ autocmd("BufWinEnter", {
         end
 
         local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
+        local opts = { buffer = bufnr, remap = false }
         vim.keymap.set("n", "<leader>p", function()
             vim.cmd.Git('push')
         end, opts)
 
         -- rebase always
         vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({'pull',  '--rebase'})
+            vim.cmd.Git({ 'pull', '--rebase' })
         end, opts)
 
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
@@ -588,8 +594,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
 -- temp theme switcher
@@ -601,19 +606,21 @@ end)
 -- treesitter
 require 'nvim-treesitter.install'.prefer_git = false
 
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "lua", "c", "vim", "query", "html", "python" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "javascript", "lua", "c", "vim", "query", "html", "python" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  highlight = { enable = true, },
-  indent = { enable = true, }
+    highlight = { enable = true, },
+    indent = { enable = true, }
 
 }
 
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
-
+-- centerpad
+vim.api.nvim_set_keymap('n', '<leader>z', "<cmd>lua require'centerpad'.toggle{ leftpad = 60, rightpad = 60 }<cr>",
+    { silent = true, noremap = true })
